efd0de97d9ac4b8ecc113e350f312fbec0d82c81
YASWANTH <Madayaswanth9@gmail.com>
2025-06-10 08:24:16 +0530

    code for +,-,*,/ operators are overloaded

--- operators_overloading.cpp
+++ operators_overloading.cpp
@@ -4,17 +4,62 @@ class overload_operator
 {
    int a;
    public:
-   void get_data(){
-    cout<<"enter a valyue :"<<endl;
-    cin>>a;
-   }
-   void put_data(){
-    cout<<"value is :"<<a<<endl;
-   }
+   void put_data();
+   void get_data();
+   overload_operator operator+(overload_operator);
+   overload_operator operator-(overload_operator);
+   overload_operator operator*(overload_operator);
+   overload_operator operator/(overload_operator);
 };
+void overload_operator::get_data(){
+cout<<"enter a valyue :";
+cin>>a;
+}
+void overload_operator::put_data(){
+cout<<"value is :"<<a<<endl;
+}
+overload_operator overload_operator::operator+(overload_operator obj2){
+    overload_operator obj3;
+    obj3.a=a+obj2.a;
+    return obj3;
+}
+overload_operator overload_operator::operator-(overload_operator obj2){
+    overload_operator obj3;
+    obj3.a=a-obj2.a;
+    return obj3;
+}
+overload_operator overload_operator::operator*(overload_operator obj2){
+    overload_operator obj3;
+    obj3.a=a*obj2.a;
+    return obj3;
+}
+overload_operator overload_operator::operator/(overload_operator obj2){
+    overload_operator obj3;
+    obj3.a=a/obj2.a;
+    return obj3;
+}
 int main(){
-    overload_operator ov;
-    ov.get_data();
-    ov.put_data();
+    overload_operator obj1,obj2,obj3;
+    obj1.get_data();
+    obj1.put_data();
+    obj2.get_data();
+    obj2.put_data();
+    obj3=obj1+obj2;
+    cout<<"---------ADD------------"<<endl;
+    obj3.put_data();
+    cout<<"------------------------"<<endl;
+     obj3=obj1-obj2;
+    cout<<"---------SUB----------"<<endl;
+    obj3.put_data();
+    cout<<"------------------------"<<endl;
+    obj3=obj1*obj2;
+    cout<<"---------MUL----------"<<endl;
+    obj3.put_data();
+    cout<<"------------------------"<<endl;
+     obj3=obj1/obj2;
+    cout<<"---------DIV--------"<<endl;
+    obj3.put_data();
+    cout<<"------------------------"<<endl;
+
     return 0;
 }
