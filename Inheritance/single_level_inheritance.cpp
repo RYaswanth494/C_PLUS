2574dc0144d069780989cd5d8dd84f5ca9ac9aed
YASWANTH <Madayaswanth9@gmail.com>
2025-06-11 08:39:40 +0530

    single level inheritance ,derived class is public mode

---
+++ Inheritance/single_level_inheritance.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class SLIB_STU{
+    private:
+    int id;
+    char name[20];
+    public:
+    void get_stu();
+    void put_stu();
+};
+void SLIB_STU::get_stu(){
+    cout<<"enter id, name :";
+    cin>>id>>name;
+}
+void SLIB_STU::put_stu(){
+    cout<<"id :"<<id<<endl;
+    cout<<"name :"<<name<<endl;
+}
+class SLIB_PHY: public SLIB_STU{
+    private:
+    float weight,height;
+    public:
+    void get_phy();
+    void put_phy();    
+};
+void SLIB_PHY::get_phy(){
+    cout<<"enter weight, height :";
+    cin>>weight>>height;
+}
+void SLIB_PHY::put_phy(){
+    cout<<"weight :"<<weight<<endl;
+    cout<<"height :"<<height<<endl;
+}
+int main(){
+    SLIB_PHY stu1;
+    stu1.get_stu();
+    stu1.get_phy();
+    stu1.put_stu();
+    stu1.put_phy();
+}
